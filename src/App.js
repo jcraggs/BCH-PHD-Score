86048de81033e262e4ae778bc601347a74edd492
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-12-19 14:58:02 +0000

    initilised project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import LiverDysfunction from "./Components/LiverDysfunction";
+import DiseaseSeverity from "./Components/DiseaseSeverity";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    AST: undefined,
+    Bilirubin: undefined,
+    PTSecs: undefined,
+    Ascites: undefined,
+    Albumin: undefined,
+    Sepsis: undefined,
+    Invasive_Access: undefined,
+    Nutrition_Support: undefined,
+    Blood_Prod_Usage: undefined,
+    Comorbidity: undefined
+  };
+
+  stateUpdater = (category, score) => {
+    console.log(category);
+    this.setState({ [category]: score });
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-Title">Paediatric Hepatology Dependency Score</h1>
+        <div className="TableContainer">
+          {/* <LiverDysfunction /> */}
+          <DiseaseSeverity stateUpdater={this.stateUpdater} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
