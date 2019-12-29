e16345baeccd411d72ca9072382cd870f6328c1e
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-12-29 20:31:57 +0000

    Email failure popup added

---
+++ src/Components/FailureMessage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+class FailureMessage extends React.Component {
+  render() {
+    return <p className="FailureMessage">Email failed to send</p>;
+  }
+}
+
+export default FailureMessage;
