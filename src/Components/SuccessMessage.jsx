0a8e0dbc198fb17629cd086fe81ed92744413165
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-12-29 20:32:36 +0000

    Email success popup added

---
+++ src/Components/SuccessMessage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+class SucccessMessage extends React.Component {
+  render() {
+    return <p className="SuccessMessage">Email sent succesfully</p>;
+  }
+}
+
+export default SucccessMessage;
